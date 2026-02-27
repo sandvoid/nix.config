9f7cbb30e8a5c0493a8f0c222cde17aa03bd9e84
Sandesh Ghimire <sandeshghimire475@gmail.com>
2026-02-27 18:33:18 +0545

    Updatig

--- configuration.nix
+++ configuration.nix
@@ -75,7 +75,7 @@
   #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      git
-     kitty
+     pkgs.alacritty
      waybar
      brightnessctl
      rofi
