b5f1e24a154cfa9d3864297c93110ea0e029da52
sandvoid <sandeshghimire475@gmail.com>
2026-06-29 10:43:42 +0545

    update

--- configuration.nix
+++ configuration.nix
@@ -10,10 +10,18 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "sandesh"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
+
+  virtualisation.virtualbox.host.enable = true;
+ 
+   hardware.enableRedistributableFirmware = true;
+   boot.kernelModules = [
+                  "rtw88_pci"
+                 "rtw88_8723de"
+                    ];
 
   # Set your time zone.
    time.timeZone = "Asia/Kathmandu";
@@ -23,7 +31,6 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
  services.xserver.xkb.layout = "us";
-
 
   # Select internationalisation properties.
    i18n.defaultLocale = "en_US.UTF-8";
@@ -36,7 +43,6 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
   
-
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
@@ -55,15 +61,24 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
+  programs.zsh = {
+  enable = true;
+  autosuggestions.enable = true;
+  syntaxHighlighting.enable = true;
+};
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.sandesh = {
      isNormalUser = true;
+     shell = pkgs.zsh;
      extraGroups = [ "wheel"
-                     "networkManager" ]; # Enable ‘sudo’ for the user.
+                     "networkManager"
+                     "wireshark" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
        tree
      ];
    };
+   
    programs.fuse.userAllowOther = true;
    programs.hyprland.enable = true;
    programs.firefox.enable = true;
@@ -75,13 +90,25 @@
    environment.systemPackages = with pkgs; [
   #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
-     kitty
-     brave
-     xfce.thunar
+     pkgs.grim
+     pkgs.qbittorrent
+     pkgs.libmtp
+     pkgs.foot
+     pkgs.iwd
+     pkgs.iw
+     pkgs.virtualbox
+     pkgs.nmap
+     pkgs.wireshark
+     pkgs.wirelesstools	
+     neovim
+     pkgs.aircrack-ng
+     tmux
+     btop
+     fastfetch
+     pkgs.brave
      zathura    
      git
-     pkgs.foot
-     pkgs.spotify
+     curl
      pkgs.python3
      liberation_ttf
      nerd-fonts.fira-code
@@ -93,7 +120,6 @@
      waybar
      brightnessctl
      rofi
-     htop
      pkgs.bleachbit
      ranger
      bibata-cursors
@@ -101,7 +127,6 @@
      wl-clip-persist
      cliphist
      pkgs.celluloid
-     neofetch
      pkgs.liberation_ttf
      pkgs.hyprlock
      pkgs.hypridle
@@ -115,7 +140,6 @@
      usbutils
      simple-mtpfs
      fuse
-     pkgs.brave
      bluez
      corefonts    
      imv
@@ -130,6 +154,7 @@
 fonts.packages = with pkgs; [
   nerd-fonts.jetbrains-mono
      corefonts
+      meslo-lgs-nf
 ];
 
 services.mysql = {
@@ -137,6 +162,7 @@ services.mysql = {
   package = pkgs.mariadb;
 };
 
+  programs.wireshark.enable = true;
 
   programs.dconf.enable = true;
 #  fonts.fontconfig.enable = true;
@@ -147,14 +173,14 @@ services.mysql = {
     GTK_CURSOR_THEME = "Adwaita"; 
   };
 
-services.gnome.gnome-keyring.enable = true;
+ # services.gnome.gnome-keyring.enable = true;
 
-security.pam.services.login.enableGnomeKeyring = true;
+ # security.pam.services.login.enableGnomeKeyring = true;
 
-services.dbus.enable = true;
+  services.dbus.enable = true;
  
- hardware.bluetooth.enable = true;
-# services.blueman.enable = true; # optional GUI
+  hardware.bluetooth.enable = true;
+ # services.blueman.enable = true; # optional GUI
 
  services.pipewire.enable = true;
  services.pipewire.pulse.enable = true;
